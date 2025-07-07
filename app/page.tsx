b06822315b493dae4fe7e07366452396c979bb03
kaoutar-12 <kmouradi@student.1337.ma>
2025-07-07 12:37:17 +0100

    deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,7 @@
 'use client'
 
 import { useReducer } from 'react'
-
-export const ACTIONS = {
-  ADD_DIGIT: 'add-digit',
-  CHOOSE_OPERATION: 'choose-operation',
-  CLEAR: 'clear',
-  DELETE_DIGIT: 'delete-digit',
-  EVALUATE: 'evaluate',
-}
+import { ACTIONS } from './constants'
 
 function reducer(state: any, { type, payload }: any) {
   switch (type) {
